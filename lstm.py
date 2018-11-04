c1e3d367c0d7839d5fb47e71dd71a46c2a2ed46d
kprakash1 <Karthik_Prakash2@intuit.com>
2018-11-04 00:33:37 -0700

    Experiment Setup

--- lstm.py
+++ lstm.py
@@ -1,11 +1,22 @@
 # load and plot dataset
 from pandas import read_csv
+import math
 from pandas import datetime
 from matplotlib import pyplot
+
 # load dataset
 series = read_csv('nyc_taxi.csv', header=0, parse_dates=[0], index_col=0, squeeze=True)
+
 # summarize first few rows
 print(series.head())
+
 # line plot
 series.plot()
 pyplot.show()
+
+# Experiment Setup 70-30
+total_records = series.size
+train_end = math.floor(total_records * 0.7)
+train_set = series[0:train_end]
+test_set = series[train_end:]
+
